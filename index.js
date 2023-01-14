a5f608782fbe7758fc110fd583f9def459933fe4
JyotiRSharma <appdeveloperubuntu@gmail.com>
2023-01-14 20:08:21 +0530

    Rename index.js to App.js

--- index.js
+++
@@ -1,9 +0,0 @@
-let heading = React.createElement("h1", {key:"0"}, "Namaste React!");
-// type of the element, props of the element, children
-let para = React.createElement("p", {key:"1"}, "Learning react from")
-let para1 = React.createElement("p", {key:"2"}, "Zero to Hero!");
-
-let container = React.createElement("div", {id:"container"}, [heading, para, para1]);
-
-let root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(container);
