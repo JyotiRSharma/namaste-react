a84dd3df2ef85c4124a2a2e25683af8cf53b1141
JyotiRSharma <appdeveloperubuntu@gmail.com>
2023-01-15 21:01:35 +0530

    Create function component with headers

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import ReactDOM  from "react-dom/client";
 const H1 = <h1 key="0">Namaste React!!</h1>
 const H2 = <h2 key="1">Learning react from</h2>
 const H3 = <h3 key="2">Zero to Hero!</h3>
-const TitleJSX = <div>{H1} {H2} {H3}</div>
+const TitleJSX = () => <div>{H1} {H2} {H3}</div>
 
 let root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(TitleJSX);
+root.render(<TitleJSX />);
