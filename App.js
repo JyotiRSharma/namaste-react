9c94e77bbade42c1305cc2b1708e6569bdb30679
JyotiRSharma <appdeveloperubuntu@gmail.com>
2023-01-15 21:04:24 +0530

    Compose components (nesting)

--- App.js
+++ App.js
@@ -1,11 +1,24 @@
 import React from "react";
-import ReactDOM  from "react-dom/client";
+import ReactDOM from "react-dom/client";
 
+const H1 = <h1 key="0">Namaste React!!</h1>;
+const H2 = <h2 key="1">Learning react from</h2>;
+const H3 = <h3 key="2">Zero to Hero!</h3>;
+const TitleJSX = () => (
+  <div>
+    {H1} {H2} {H3}
+  </div>
+);
 
-const H1 = <h1 key="0">Namaste React!!</h1>
-const H2 = <h2 key="1">Learning react from</h2>
-const H3 = <h3 key="2">Zero to Hero!</h3>
-const TitleJSX = () => <div>{H1} {H2} {H3}</div>
+const Header = () => {
+  return (
+    <>
+      <h1>Inside Header component</h1>
+      <hr />
+      <TitleJSX />
+    </>
+  );
+};
 
 let root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<TitleJSX />);
+root.render(<Header />);
