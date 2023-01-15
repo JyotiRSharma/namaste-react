80c4e27f610d5011eded1b31851dec2d79d8aa0f
JyotiRSharma <appdeveloperubuntu@gmail.com>
2023-01-15 16:17:18 +0530

    Import react code

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 import React from "react";
-import ReactDOM  from "react-dom";
+import ReactDOM  from "react-dom/client";
 
-console.log("hi");
+
 let heading = React.createElement("h1", {key:"0"}, "Namaste React!");
 // type of the element, props of the element, children
 let para = React.createElement("p", {key:"1"}, "Learning react from")
