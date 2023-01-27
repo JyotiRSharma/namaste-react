0e992ea19b97ade9ea224b4d92357491f5bdc5e4
JyotiRSharma <appdeveloperubuntu@gmail.com>
2023-01-27 23:37:12 +0530

    Remove search logic from Body

--- src/components/Body.js
+++ src/components/Body.js
@@ -2,10 +2,9 @@ import React from "react";
 import RestaurantList from "./RestaurantList";
 
 const Body = () => {
+
   return (
     <>
-      <input type="text" className="search-text" placeholder="Search" value="" />
-      <button type="submit">Search</button>
       <RestaurantList />
     </>
   );
