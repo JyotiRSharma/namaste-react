6f8ae98c00a62bed57ebf6bb35667bbe14af0e63
JyotiRSharma <appdeveloperubuntu@gmail.com>
2023-01-28 00:00:23 +0530

    Add search restaurant functionality

--- src/components/RestaurantList.js
+++ src/components/RestaurantList.js
@@ -2,9 +2,19 @@ import RestaurantCard from "./RestaurantCard.js";
 import { RESTAURANT_LIST_DATA } from "../../config.js";
 import { useState } from "react";
 
+const filterRestuarant = (searchText, restaurantList) => {
+  return restaurantList.filter((restaurant) => {
+    if(restaurant?.data?.name) {
+      return restaurant.data.name.includes(searchText);
+    }
+  });
+};
+
 const RestaurantList = () => {
   const [searchText, setSearchText] = useState("");
-  const [buttonStatus, setButtonStatus] = useState("false");
+  const [restaurantList, setRestaurantList] = useState(RESTAURANT_LIST_DATA);
+
+  console.log(restaurantList);
   return (
     <>
       <input
@@ -14,18 +24,26 @@ const RestaurantList = () => {
         value={searchText}
         onChange={(e) => {
           setSearchText(e.target.value);
+          setRestaurantList(filterRestuarant(searchText, restaurantList));
+          if (searchText.length <= 1) {
+            setRestaurantList(RESTAURANT_LIST_DATA);
+          }
         }}
       />
-      <button type="submit" onClick={() => {
-        buttonStatus === "false" ? setButtonStatus("true") : setButtonStatus("false");
-      }}>{buttonStatus}</button>
+      <button
+        type="submit"
+        onClick={() => {
+          setRestaurantList(filterRestuarant(searchText, restaurantList));
+        }}
+      >
+        Search
+      </button>
       <div className="restaurant-list">
-        {RESTAURANT_LIST_DATA.map((item) => {
+        {restaurantList.map((item) => {
           return <RestaurantCard {...item.data} key={item.data.uuid} />;
         })}
       </div>
     </>
-
   );
 };
 
